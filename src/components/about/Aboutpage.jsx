0e637739dbc7c1eead835109813a8b4b43d3aa36
Mathias <MathiasAsBirkeland@outlook.dk>
2025-11-04 00:37:13 +0100

    lille fix med about

--- src/components/about/Aboutpage.jsx
+++ src/components/about/Aboutpage.jsx
@@ -17,14 +17,6 @@ const AboutPage = () => {
             Hi, I’m Mathias! Traveling has always been a passion of mine, and this road trip gave me the chance to explore the beauty of Italy and Croatia. From indulging in authentic Italian pasta to wandering through Venice’s canals, every moment was incredible. 
           </p>
         </div>
-
-        <div className="about-person">
-          <img src="../src/assets/Black.webp" alt="Oliver" className="about-image" />
-          <h2 className="about-name">Oliver</h2>
-          <p className="about-text">
-            Hey, I’m Oliver! This journey was about discovering new places, experiencing different cultures, and of course, eating amazing food! From the vibrant streets of Milan to the historic charm of Rijeka, this trip was truly special.
-          </p>
-        </div>
       </div>
     </div>
   );
